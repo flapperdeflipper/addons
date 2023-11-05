37bbe1f52b78d2a2aeefc78087914889d5906abf
Flip <flip.hess@tiqets.com>
2023-11-05 21:47:57 +0100

    Bump version

--- willow-was/rootfs/init.sh
+++ willow-was/rootfs/init.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -e -o pipefail +x
 
-echo "--> Creating /share/willow/was directories"
+function log() {
+    local severity="${1}"; shift
+    local message="${*}"
+    echo "$( date ) - ${severity} - ${message}" >&2
+}
+
+log INFO "Creating /share/willow/was directories"
 mkdir -p /share/willow/was/{storage,firmware} || exit 1
 
-echo "--> Copying files from /app/storage to /share/willow/was/storage"
+log INFO "Copying files from /app/storage to /share/willow/was/storage"
 cp -rv /app/storage/* /share/willow/was/storage/ || exit 1
 
-echo "--> Symlinking /app/storage to /share/willow/was/storage/"
+log INFO "Symlinking /app/storage to /share/willow/was/storage/"
 rm -rf /app/storage                     || exit 1
 ln -sf /share/willow/was/storage /app/  || exit 1
 
-echo "--> Running /app/entrypoint.sh"
+log INFO "Running /app/entrypoint.sh"
 exec /app/entrypoint.sh
