9ae312c807a1ce2742c4a0c00b5db902bce6c613
Flip <flip.hess@tiqets.com>
2023-11-05 18:38:36 +0100

    Fix entrypoint?

--- willow-was/rootfs/init.sh
+++ willow-was/rootfs/init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e -o pipefail +x
 
+echo "--> Creating /share/willow/was directories"
 mkdir -p /share/willow/was/{storage,firmware} || exit 1
 
-## Create symlink to share storage for storage directory
-if [[ ! -L /app/storage ]]
-then
-    cp -rv /app/storage/* /share/willow/was/storage || exit 1
-    ln -sf /share/willow/was/storage /app/storage   || exit 1
-fi
+echo "--> Copying files from /app/storage to /share/willow/was/storage"
+cp -rv /app/storage/* /share/willow/was/storage/ || exit 1
 
+echo "--> Symlinking /app/storage to /share/willow/was/storage/"
+ln -sf /share/willow/was/storage /app/  || exit 1
+
+echo "--> Running /app/entrypoint.sh"
 exec /app/entrypoint.sh
